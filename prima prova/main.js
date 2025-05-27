198096d54f79a65380ba26247a3795f09700e06b
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-05-27 16:36:04 +0200

    (Add HTML structure and JavaScript for travel cost calculation with age-based discounts for second try)

--- prima prova/main.js
+++ prima prova/main.js
@@ -13,35 +13,35 @@ TIP: per controllare che la vostra logica sui prezzi funzioni correttamente, pro
 // Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
 const distance = Number(prompt('Scrivi il numero dei chilometri che vuoi percorrere'));
 const passengerAge = Number(prompt("Scrivi l'età del passeggero"));
-console.log(distance, passengerAge);
+// console.log(distance, passengerAge);
 
 
 /* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km) */
-const priceForKm = Number(0.21);
+const priceForKm = 0.21;
 const price = distance * priceForKm;
-const priceFixed = Number(price.toFixed(2));
+// const priceFixed = Number(price.toFixed(2));
 // console.log(priceFixed);
 
 
 // va applicato uno sconto del 20% per i minorenni
-const percentageDiscount18 = Number(20);
-const under18 = Number(priceFixed * (1 - (percentageDiscount18 / 100)));
-const under18Fixed = Number(under18.toFixed(2));
+const percentageDiscount18 = 20;
+const under18 = price * (1 - (percentageDiscount18 / 100));
+// const under18Fixed = Number(under18.toFixed(2));
 // console.log(under18Fixed);
 
 // va applicato uno sconto del 40% per gli over 65. */
-const percentageDiscount65 = Number(40);
-const over65 = priceFixed * Number((1 - (percentageDiscount65 / 100)));
-const over65Fixed = Number(over65.toFixed(2));
+const percentageDiscount65 = 40;
+const over65 = price * (1 - (percentageDiscount65 / 100));
+// const over65Fixed = Number(over65.toFixed(2));
 // console.log(over65Fixed);
 
 
 
 if (passengerAge < 18) {
-    console.log(under18Fixed)
+    alert(under18.toFixed(2))
 } else if (passengerAge > 65) {
-    console.log(over65Fixed)
+    alert(over65.toFixed(2))
 } else {
-    console.log(priceFixed)
+    alert(price.toFixed(2))
 }
